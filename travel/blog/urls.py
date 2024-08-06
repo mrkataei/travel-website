3870f1e52a1c37d19e745f45b183108e4f67189e
mrkataei <mrkataei@gmail.com>
2024-08-07 00:05:46 +0330

    add kwargs to blog view / add author filter view

--- travel/blog/urls.py
+++ travel/blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', blog, name='blog'),
     # path('category/<str:cat_name>', blog_category, name='category'), # exampla when we two vies for this aim
     path('category/<str:cat_name>', blog, name='category'),
+    path('author/<str:author_username>', blog, name='author'),
     path('post-<int:pid>', blog_single, name='single'),
 ]
--- travel/blog/views.py
+++ travel/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def blog(request, cat_name = None):
+# def blog(request, cat_name = None, author_username = None):
+def blog(request, **kwargs):
     posts = Post.objects.filter(status=1)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'] )
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context=context)
 
