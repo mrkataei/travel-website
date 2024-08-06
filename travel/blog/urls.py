f67b59ebd81ad5270781128911a9cd3aebcf4f0e
mrkataei <mrkataei@gmail.com>
2024-08-06 23:29:21 +0330

    add functional category element - show home blog category/<str:cat_name> in

--- travel/blog/urls.py
+++ travel/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import blog, blog_single
+from blog.views import blog, blog_single, blog_category
 
 app_name = 'blog'
 urlpatterns = [
     path('', blog, name='blog'),
+    path('category/<str:cat_name>', blog_category, name='category'),
     path('post-<int:pid>', blog_single, name='single'),
 ]
--- travel/blog/views.py
+++ travel/blog/views.py
@@ -12,3 +12,8 @@ def blog_single(request, pid):
     return render(request, 'blog/blog-single.html', context=context)
 
 
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name) # catgory -> get us id -> with __ we can extract fields like name
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context=context)
