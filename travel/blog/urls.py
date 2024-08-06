aa3787b7912ef5bda95e71b5eb8eafcab854cbc6
mrkataei <mrkataei@gmail.com>
2024-08-06 23:44:27 +0330

    join cat view and blog home

--- travel/blog/urls.py
+++ travel/blog/urls.py
@@ -4,6 +4,7 @@ from blog.views import blog, blog_single, blog_category
 app_name = 'blog'
 urlpatterns = [
     path('', blog, name='blog'),
-    path('category/<str:cat_name>', blog_category, name='category'),
+    # path('category/<str:cat_name>', blog_category, name='category'), # exampla when we two vies for this aim
+    path('category/<str:cat_name>', blog, name='category'),
     path('post-<int:pid>', blog_single, name='single'),
 ]
--- travel/blog/views.py
+++ travel/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def blog(request):
+def blog(request, cat_name = None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context=context)
 
@@ -12,7 +14,7 @@ def blog_single(request, pid):
     return render(request, 'blog/blog-single.html', context=context)
 
 
-def blog_category(request, cat_name):
+def blog_category(request, cat_name): # we can use just one view def blog
     posts = Post.objects.filter(status=1)
     posts = posts.filter(category__name=cat_name) # catgory -> get us id -> with __ we can extract fields like name
     context = {'posts': posts}
